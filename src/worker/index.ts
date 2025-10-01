61aa1b3f82e6c434c41b067a5359ee9ca7b9902e
Dantton Lavigne <79911086+9cs@users.noreply.github.com>
2025-10-01 11:38:04 -0300

    'a'

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,5 +1,14 @@
 import { Hono } from "hono";
 
-const app = new Hono<{ Bindings: Env }>();
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.text(`Hello from Vercel!`);
+});
+
+app.get("/db", (c) => {
+  const dbUrl = process.env.DB_URL || "DB_URL not set";
+  return c.text(`Database URL: ${dbUrl}`);
+});
 
 export default app;
